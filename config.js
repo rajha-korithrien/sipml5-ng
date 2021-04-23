09a4b5988f9f400e91207724f0962afa9623ba8c
Max Planck <mplanck@icasa.nmt.edu>
2021-04-23 14:35:39 -0600

    Added the initial stab at loading configuration from a server side file.

---
+++ config.js
@@ -0,0 +1,13 @@
+var initialConfig={
+    websocketUrl : "some url",
+    sipRealm : "some realm",
+    disableVideo : true,
+    enableBreakerHacks : false,
+    outboundProxyUrl : "",
+    iceServersJson : "[]",
+    maxBandwidthJson : "{audio:64}",
+    disable3GPP : true,
+    disableDebugMessages : true,
+    cacheMediaStream : false,
+    disableCallButtonOptions : true
+}
